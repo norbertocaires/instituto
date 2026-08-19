4676d5723be244a16fd1891fb4935e843bb2e734
norbertocaires <beto_caires@hotmail.com>
2026-08-19 15:26:49 -0300

    teste

--- src/app/components/contact-info/contact-info.component.ts
+++ src/app/components/contact-info/contact-info.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { SchoolDataService } from '../../services/school-data.service';
 import { SchoolInfo } from '../../models';
 
@@ -13,17 +12,11 @@ import { SchoolInfo } from '../../models';
 })
 export class ContactInfoComponent {
   private schoolDataService = inject(SchoolDataService);
-  private sanitizer = inject(DomSanitizer);
 
   schoolInfo: SchoolInfo = this.schoolDataService.getSchoolInfo();
 
   get mapsSearchLink(): string {
     return `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(this.schoolInfo.endereco)}`;
-  }
-
-  get mapEmbedUrl(): SafeResourceUrl {
-    const url = `https://www.google.com/maps?q=${encodeURIComponent(this.schoolInfo.endereco)}&output=embed`;
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
   get whatsAppLink(): string {
